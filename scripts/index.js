8b70cd9185b37929f605ab639994a38ee217be58
nardo88 <nardo1988@mail.ru>
2021-05-05 22:15:52 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -15,34 +15,57 @@ document.addEventListener('DOMContentLoaded', () => {
     const lines = () => {
         class LineMove {
             constructor(selector) {
-                this.items = document.querySelector(selector).children;
+                this.items = document.querySelector(selector);
                 this.count = 0;
+                this.widthWindow = 600;
             }
 
             init() {
+
+
+                for(let i = 0; i < 7; i++){
+                    const elem = document.createElement('div');
+                    elem.classList.add('lines__elem');
+                    
+                    elem.innerHTML = `
+                        <img src="./img/lines/warning.svg" alt="warning">
+                        <span class="lines__text">Вход строго по приглашениям </span>
+                    `;
+                    
+                    elem.left = 0;
+                    this.items.insertAdjacentElement('beforeend', elem);
+
+                   
+                }
 
                requestAnimationFrame(this.move.bind(this))
             }
 
             move(){
 
-                this.count++;
-                console.log(this.count);
-                [...this.items].forEach(elem => {
-                    elem.style.transform = `translateX(${this.count}px)`;
-                    if (parseFloat( elem.style.transform.split('(')[1]) > 300){
-                        elem.parentElement.prepend(elem)
+                
+                [...this.items.children].forEach(elem => {
+                    elem.left ++;
+                    elem.style.transform = `translateX(${elem.left}px)`;
+                    if (elem.left > this.widthWindow){
+                        elem.parentElement.insertAdjacentElement('afterbegin', elem)
+                        elem.left = 0;
+                        elem.style.transform = `translateX(${elem.left}px)`;
                     }
                 })
-                // requestAnimationFrame(this.move.bind(this))
+                requestAnimationFrame(this.move.bind(this));
                 
 
             }
+
+           
         }
 
         const line1 = new LineMove('.first-line');
+        const line2 = new LineMove('.second-line');
 
         line1.init();
+        line2.init();
 
         
 
