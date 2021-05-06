d7267daf1dacb2f99cad6078702c39047af4ff8d
nardo88 <nardo1988@mail.ru>
2021-05-06 20:49:41 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menu = () => {
-        const burger = document.querySelector('.burger');
-        const nav = document.querySelector('.nav');
 
-        burger.addEventListener('click', e => {
-            nav.classList.toggle('open')
+        const header = document.querySelector('.header');
+        const nav = document.querySelector('.nav');
+        const overlayReg = document.querySelector('.overlay-reg'); 
+        const overlayThank = document.querySelector('.overlay-thank'); 
+
+        // функция открыти - закрытия меню
+        const toggleMenu = () => {
+            nav.classList.toggle('open');
+        }
+
+        
+
+        // функция скрола
+        const skroll = (selector) => {
+            // получаем высоту раздела
+            const scrollHeight = document.querySelector(selector).offsetTop;
+            window.scrollTo({top: scrollHeight, behavior: 'smooth'});
+            toggleMenu();
+        }
+
+        overlayReg.addEventListener('click', e => {
+            if (e.target.classList.contains('visible')){
+                overlayReg.classList.remove('visible');
+            }
         })
+
+        header.addEventListener('click', e => {
+            const target = e.target;
+
+            // отркываем модалку с регистрацией
+            if(target.classList.contains('nav__registration')){
+                toggleMenu();
+                overlayReg.classList.add('visible');
+            }
+            // открытие меню кликая по бергеру
+            if(target.closest('.burger')){
+                toggleMenu();
+            }
+
+            // скролл
+            if (target.closest('.nav__item')){
+                e.preventDefault();
+                skroll(target.closest('.nav__item').dataset.scroll);
+            }
+        })
+
+
+        // закрытие модалки thank по клику
+        overlayThank.addEventListener('click', e => {
+            const target = e.target;
+            
+            if (target.classList.contains('overlay-thank')){
+                overlayThank.classList.remove('visible');
+            }
+
+            if (target.classList.contains('modal-thank__btn')){
+                overlayThank.classList.remove('visible');
+            }
+        })
+
+        // ====================== отправка формы ========================\
+
+
+
+       
+
+        // say thank
+        const thank = () => {
+            
+            overlayThank.classList.add('visible');
+
+            setTimeout(() => {
+                overlayThank.classList.remove('visible');
+            }, 5000)
+        }
+
+
+        document.addEventListener('submit', e => {
+            e.preventDefault();
+            overlayReg.classList.remove('visible');
+           
+            thank();
+        })
+
 
     }
 
@@ -56,4 +135,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     lines();
+
+
+    
+
+
+   
 })
